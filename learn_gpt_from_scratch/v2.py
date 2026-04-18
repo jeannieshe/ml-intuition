71f248fb2f75cf9357bb0d630fdde69b50895ccb
Jeannie She <jeanshe@mit.edu>
2026-04-18 16:57:49 -0400

    change parameters to scale up gpt

--- learn_gpt_from_scratch/v2.py
+++ learn_gpt_from_scratch/v2.py
@@ -3,17 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
-lr = 1e-3
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+lr = 3e-4
 max_iters = 5000
-eval_interval = 300
+eval_interval = 500
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
-n_head = 4
-n_layer = 4
-dropout = 0.3
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 # reproducibility
 torch.manual_seed(1337)
